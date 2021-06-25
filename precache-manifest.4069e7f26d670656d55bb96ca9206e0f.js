5c4a09c10f7b627ccd1f4722f0b297a048a0d391
nikita11044 <nikita11042000@gmail.com>
2021-06-25 16:15:16 +0300

    Updates

---
+++ precache-manifest.4069e7f26d670656d55bb96ca9206e0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7297f637c22065c84ac75d73bba50841",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "557c1b67f57c7d7b148c",
+    "url": "/ToDoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "c54787b64f522eebf76b",
+    "url": "/ToDoList/static/js/2.985f8dbb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/ToDoList/static/js/2.985f8dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557c1b67f57c7d7b148c",
+    "url": "/ToDoList/static/js/main.61c462a1.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
